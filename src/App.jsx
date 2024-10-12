61e5b8c4fbc3400a43b463720d8d83f697bf9fb5
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-10-12 15:42:32 +0300

    day 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
-import { Counter } from "./Components/Counter";
+// import { Counter } from "./Components/Counter";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Error from "./Pages/Error";
 
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Counter />
-    </>
+    <BrowserRouter>
+      <nav>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <Link className="navbar-brand" to="/">
+                Home
+              </Link>
+              <Link className="navbar-brand" to="/about">
+                About
+              </Link>
+              <Link className="navbar-brand" to="/contact">
+                Contact
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function Counter() {
-  const [state, setState] = useState({ count: 0, name: "ahmed" });
-  const increment = () => setState({ ...state, count: state.count + 1 });
+  const [timer, setTimer] = useState(0);
+  // const [count1, setCount1] = useState(0);
+  // const increment1 = () => setCount1(count1 + 1);
+
+  // const [count2, setCount2] = useState(0);
+  // const increment2 = () => setCount2(count2 + 1);
+  // clearInterval;
+  useEffect(() => {
+    const incrementTimer = () => {
+      // setTimer(timer + 1);
+    };
+    const interval = setInterval(incrementTimer, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  // useEffect(() => {
+  //   console.log("useEffect");
+  // });
+  console.log("render");
 
   return (
     <>
-      <h2>Name :{state.name}</h2>
-      <h1>Count :{state.count}</h1>
-      <button onClick={increment}>+</button>
+      <h1>Timer:{timer}</h1>
+      {/* <button onClick={() => setTimer(timer + 1)}>+</button> */}
+      {/* <h1>Count :{count1}</h1>
+      <button onClick={increment1}>+</button>
+      <h1>Count :{count2}</h1>
+      <button onClick={increment2}>+</button> */}
     </>
   );
 }
